aeb16c1f5cac2c7f6f3938b5a3932fff6fd7b311
suhanlee <suhan.lee.k@gmail.com>
2015-10-13 02:02:26 +0900

    initial commit

---
+++ app/scripts/main.js
@@ -0,0 +1,18 @@
+/*global Askfm, $*/
+
+
+window.Askfm = {
+    Models: {},
+    Collections: {},
+    Views: {},
+    Routers: {},
+    init: function () {
+        'use strict';
+        console.log('Hello from Backbone!');
+    }
+};
+
+$(document).ready(function () {
+    'use strict';
+    Askfm.init();
+});
